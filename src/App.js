798c2c41a9b3cf9482b7a57aef10b93b87a90b7c
Varya Stepanova <ext-varya.stepanova@elisa.fi>
2019-11-06 14:46:03 +0200

    Show form (everything breaks)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { MyForm } from 'lib-with-formik'
+
 function App() {
   return (
     <div className="App">
-      Hello world
+      <MyForm/>
     </div>
   );
 }
